f6837c2d308e2e44a3cd53542dd492e5efb36f1e
juj624 <julienpenot@outlook.com>
2021-07-19 17:24:20 +0200

    elsy2

--- reactchapitre_01/elsy/src/App.js
+++ reactchapitre_01/elsy/src/App.js
@@ -12,24 +12,65 @@ const stepsMin = 0;
 const stepsMax = 50000;
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      water: "1.5",
+      heart: "120",
+      temperature: "-10",
+      steps: "3000",
+    };
+
+    this.onHeartChange = this.onHeartChange.bind(this)
+    this.onStepsChange = this.onStepsChange.bind(this)
+    this.onTempChange = this.onTempChange.bind(this)
+     
+  }
+
+  onHeartChange(e) {
+   
+    this.setState({heart: e.target.value})
+    this.calculateWeather()
+  }
+
+
+  onStepsChange(e) {
+   
+    this.setState({steps: e.target.value})
+    this.calculateWeather()
+  }
+
+  onTempChange(e) {
+   
+    this.setState({temperature: e.target.value})
+    this.calculateWeather()
+  }
+
+  calculateWeather(e){
+    
+  }
+  
+
   render() {
     return (
       <div className="container-fluid">
         <div className="row">
-          <p>Heart: {heartMin}</p>
+          {/* <p>Heart: {heartMin}</p>
           <p>Temperature: {tempMin}</p>
-          <p>Steps: {stepsMin}</p>
+          <p>Steps: {stepsMin}</p> */}
 
           {/* water */}
-          <Box icon="local_drink" unit="L" color="#3A85FF" value={1.5} />
+          <Box icon="local_drink" unit="L" color="#3A85FF" value={this.state.water} />
           {/* steps */}
-          <Box icon="directions_walk" color="black" value={3000} unit="steps" />
+          <Box icon="directions_walk" color="black" value={this.state.steps} unit="steps" min={stepsMin} max={stepsMax} onChange={this.onStepsChange} />
           {/* heart */}
-          <Box icon="favorite" color="red" value={120} unit="bpm" />
+          <Box icon="favorite" color="red" value={this.state.heart} unit="bpm" min={heartMin} max={heartMax} onChange={this.onHeartChange}/>
           {/* temperature */}
-          <Box icon="wb_sunny" color="yellow" value={-10} unit="°C" />
+          <Box icon="wb_sunny" color="yellow" value={this.state.temperature} unit="°C" min={tempMin} max={tempMax} onChange={this.onTempChange} />
         </div>
       </div>
+    
     );
   }
 }
--- reactchapitre_01/elsy/src/componements/Box.js
+++ reactchapitre_01/elsy/src/componements/Box.js
@@ -6,11 +6,20 @@ import React from "react";
 // }
 
 class Box extends React.Component {
+  
   render() {
+    const vlc = () =>{
+     if(this.props.icon !== "local_drink"){
+        return <input type="range" 
+        min={this.props.min} max={this.props.max} value =    {this.props.value} onChange={this.props.onChange}>
+         </input>
+    }
+    }
     return (
-      <div className="box col-sm-3 col-6">
+      <div className="box col-sm-3 col-6 ">
         <span className="material-icons" style={{color: this.props.color, fontSize: 100,}}>{this.props.icon}</span>
         <p>{this.props.value}{this.props.unit}</p>
+        {vlc()}
       </div>
     );
   }

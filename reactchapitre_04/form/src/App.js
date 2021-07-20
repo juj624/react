a89659ee4d92cac6a3a4f2251958461ae79a2862
juj624 <julienpenot@outlook.com>
2021-07-20 17:26:06 +0200

    form

---
+++ reactchapitre_04/form/src/App.js
@@ -0,0 +1,61 @@
+import React from "react"
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css"
+
+
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      email: "",
+      password: "",
+      isValidEmail: false,
+      isValidPassword: false,
+
+    }
+    this.onEmailChange = this.onEmailChange.bind(this)
+    this.onPasswordChange = this.onPasswordChange.bind(this)
+
+  }
+  onEmailChange(e) {
+    this.setState({ email: e.target.value })
+
+  }
+
+  onPasswordChange(e) {
+    this.setState({ password: e.target.value })
+    if (e.target.value.length >= 6) {
+      this.setState({ isValidPassword: true })
+    } else {
+      this.setState({ isValidPassword: false })
+    }
+  }
+
+  render() {
+
+    return (
+      <form>
+        <div className="form-group row d-flex p-2 bd-highlight d-flex justify-content-center">
+          <div className="col-sm-12">
+            <label for="inputEmail" className="col-sm-12 col-form-label">Email</label>
+            <input type="text" className="form-control" id="inputEmail" placeholder="julienpenot@outook.com" value={this.state.email} onChange={this.onEmailChange} />
+          </div>
+        </div>
+        <div className="form-group row d-flex p-2 bd-highlight d-flex justify-content-center">
+          <div className="col-sm-12">
+            <label for="inputPassword" className="col-sm-12 col-form-label">Password</label>
+            <input type="password" className={this.state.isValidPassword ? "is-valid form-control" : "is-invalid form-control"} id="inputPassword" placeholder="Password" value={this.state.password} onChange={this.onPasswordChange} />
+          </div>
+        </div>
+        <button className="btn btn-primary" type="submit">Submit form</button>
+      </form>
+    );
+  }
+
+
+
+
+}
+
+export default App;
